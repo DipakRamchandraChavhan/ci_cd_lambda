cbcdb2a4b0c8d215ccc215124e21b13764c12a24
deepak <dipak.chavhan51@gmail.com>
2023-08-09 12:43:07 +0530

    updated lambda function to list s3 buckets

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,15 @@
+import boto3
+
 def lambda_handler(event, context):
-    print('Hello World')
-    print('Updated Hello World')
+    print('Hello World start')
+        
+    s3_client = boto3.client('s3')
+
+    response = s3_client.list_buckets()
+    
+    buckets=response['Buckets']
+
+    for bucket in buckets:
+        print(bucket['Name'])
+        
+    print('Hello World end')
